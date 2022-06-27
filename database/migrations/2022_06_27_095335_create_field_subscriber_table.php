48ff6e3452820f3444bed86aed43e580ff2d5caa
somtee99 <okaforsomtoo007@gmail.com>
2022-06-27 10:54:48 +0100

    Many to many relationship added

---
+++ database/migrations/2022_06_27_095335_create_field_subscriber_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('field_subscriber', function (Blueprint $table) {
+            $table->id();
+            $table->integer('field_id');
+            $table->integer('subscriber_id');
+            $table->string('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('field_subscriber');
+    }
+};
